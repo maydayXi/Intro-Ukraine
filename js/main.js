2433ff751044b789625b131fa392e8968d7070ec
James Frences Ryan <onepiece762@gmail.com>
2022-03-13 03:21:16 +0800

    Update main.js
    Add blockUI Effect

--- js/main.js
+++ js/main.js
@@ -23,9 +23,42 @@ $(document).ready(function () {
 
 	};
 
+	var preload = function () {
+		var titleStyle = 
+			'top: 50%; transform: translateY(-50%); font-size: 3rem; letter-spacing: 10px;';
+  		var title = '獻給所有為自由<br/>和平奮鬥的每一個你';
+  		var blockTitle = `<h1 style='${titleStyle}'>${title}</h1>`;
+
+		$.blockUI({
+			css: {
+			    border: 'none',
+			    padding: '2rem',
+			    backgroundColor: '#000000',
+			    color: '#FFFFFF',
+			    fontSize: '2rem',
+			    letterSpacing: '5px',
+			    width: '100%',
+			    height: '100%',
+			    left: '0%',
+			    top: '0%',
+			    margin: 0,
+			    padding: 0
+			},
+			message: blockTitle,
+			onBlock: () => $('body').css('overflow', 'hidden')
+		});
+
+		setTimeout(function () {
+			$.unblockUI({
+				onUnblock: () => $('body').css('overflow', '')
+			});
+		}, 4000);
+	}
+
 	var skr = skrollr.init();
 	var wow = new WOW();
 	wow.init();
 
 	$WIN.scroll(toggleNavbar);
+	$WIN.on('load', preload);
 });
