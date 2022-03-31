6e31e2206f93923a1fe18e649654b1efc450b1d8
James Frences Ryan <onepiece762@gmail.com>
2022-04-01 00:41:04 +0800

    Update Culture section
    update ScrollMagic animation effect

--- js/main.js
+++ js/main.js
@@ -3,14 +3,27 @@ $(document).ready(function () {
 	var $Navbar 	= $('#Navbar');
 	var $Navbtn		= $Navbar.find('button');
 	var $NavLink	= $('#NavbarLinks');
+	var $Intro 		= $('#Intro');
+	var $Map 			= $('#Map');
+	var $City 		= $('#City');
+	var $Place 		= $('#Place');
+	var $Create		= $('#Create');
+	var $Food			= $('#Food');
+	var $Wine 		= $('#Wine');
 
-	const e 		= 'click';
-	const h 		= 'height';
-	const bdb		= 'border-bottom';
+	const e 			= 'click';
+	// CSS Attr
+	const h 			= 'height';
+	const bdb			= 'border-bottom';
 
-	const INTRO 		= '#Intro';
-	const INTROIMG	= '#Img-Intro';
-	const MAP				= '#Map';	
+	// Animate Const 
+	const opValue = 0.3;
+	const ptValue = "150%";
+
+	var scene_obj = {};
+
+	// init controller
+	var sceneController = new ScrollMagic.Controller();
 
 	// Change the navbar theme
 	var toggleNavbar = function () {
@@ -30,8 +43,8 @@ $(document).ready(function () {
 	var preload = function () {
 		var titleStyle = 
 			'top: 50%; transform: translateY(-50%); font-size: 3rem; letter-spacing: 10px;';
-  		var title = '獻給所有為自由<br/>和平奮鬥的每一個你';
-  		var blockTitle = `<h1 style='${titleStyle}'>${title}</h1>`;
+		var title = '獻給所有為自由<br/>和平奮鬥的每一個你';
+		var blockTitle = `<h1 style='${titleStyle}'>${title}</h1>`;
 
 		$.blockUI({
 			css: {
@@ -57,33 +70,129 @@ $(document).ready(function () {
 				onUnblock: () => $('body').css('overflow', '')
 			});
 		}, 4000);
+
+		animate();
 	};
 
+	// Animate Function 
+	var animate = function () {
+		introAnimate();
+		mapAnimate();
+		cultureAnimate();
 
-	var introTween = TweenMax.to(INTROIMG, 1, {
-		css: {opacity: 1},
-		ease: "none"
-	});
+		sceneController.addScene([
+			scene_obj.IntroContnet,
+			scene_obj.introImg,
+			scene_obj.Map
+		]);
 
-	var mapTween = TweenMax.to(MAP, 1, {
-		css: {backgroundSize: "200%"},
-		ease: Power1.out
-	});
+		console.log(scene_obj);
+	};
 
-	// init controller
-	var scrollController = new ScrollMagic.Controller();
-	// Intro image Effect
-	var IntroScene = new ScrollMagic.Scene({
-		triggerElement: INTRO,
-		triggerHoot: "onEnter"
-	}).setTween(introTween);
-	// Map Effect
-	var MapScene = new ScrollMagic.Scene({
-		triggerElement: MAP,
-		triggerHoot: "onEnter"
-	}).setTween(mapTween);
+	// Intro Animate
+	var introAnimate 	= function () {
+		let introContent 	= $Intro.find('.intro')[0];
+		let introImg 			= $Intro.find('.img-intro')[0];
 
-	scrollController.addScene([IntroScene, MapScene]);
+		var introTween 		= TweenMax.from(introContent, 1.5, {
+			css: { opacity: 0, paddingTop: ptValue },
+			delay: 1,
+			ease: Power2.out
+		});
+		var introImgTween = TweenMax.from(introImg, 1.5, {
+			css: { opacity: opValue, transform: "translateY(-80px)"  },
+			ease: Power2.out
+		});
+
+		var introScene 		= new ScrollMagic.Scene({
+			triggerElement: $Intro[0],
+			triggerHook: 0.5,
+			offset: -100,
+			duration: 250
+		}).setTween(introTween);
+		var introImgScene = new ScrollMagic.Scene({
+			triggerElement: $Intro[0],
+			triggerHook: 0.8,
+			offset: 200,
+			duration: 250
+		}).setTween(introImgTween);
+
+		scene_obj.IntroContnet 	= introScene;
+		scene_obj.introImg 			= introImgScene; 
+	};
+
+	// map scale animation
+	var mapAnimate 		= function () {
+		var mapTween = TweenMax.from($Map[0], 2, {
+			css: { backgroundSize: "300%" },
+			ease: Power2.out
+		});
+		var mapScene = new ScrollMagic.Scene({
+			triggerElement: $Map[0],
+			triggerHook: 0.4,
+			duration: 200
+		}).setTween(mapTween);
+
+		scene_obj.Map = mapScene;
+	};
+
+	// culture animation 
+	var cultureAnimate = function () {
+		var cultures = [];
+		cultures.push(
+			$City, $Place, $Create, $Food, $Wine
+		);
+
+		$.each(cultures, (index, item) => {
+			var title = $(item).find('.title')[0];
+			var img 	= item.find('.img')[0];
+			var intro = item.find('.intro')[0];
+			var btn 	= item.find('.btn')[0];
+
+			console.log($(item)[0]);
+
+			var opTween = TweenMax.from([title, img], 1.5, {
+				css: { opacity: opValue },
+				ease: Power2.out
+			});
+
+			var temp  = '';
+			switch(index) {
+				case 0:
+				case 2:
+				case 4:
+					temp = "-20%";
+					break;
+				case 1:
+				case 3:
+					temp = "20%";
+					break;
+			}
+
+			var introTween = TweenMax.from(intro, 1.5, {
+				css: { opacity: opValue, left: temp },
+				ease: Power2.out
+			});
+
+			var btnTween 	= TweenMax.from(btn, 2, {
+				css: { opacity: opValue, left: temp },
+				ease: Power2.out
+			});
+
+			new ScrollMagic.Scene({
+				triggerElement: $(item)[0]
+			}).setTween(opTween).addTo(sceneController);
+
+			new ScrollMagic.Scene({
+				triggerElement: $(item)[0]
+			}).setTween(introTween).addTo(sceneController);
+
+			new ScrollMagic.Scene({
+				triggerElement: $(item)[0],
+				offset: 50
+			}).setTween(btnTween).addTo(sceneController);
+		});
+	};
 
 	// init animation effect 
 	var wow = new WOW();
